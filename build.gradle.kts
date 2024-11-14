a38b65ba8474d6d9ce85ca76bc7e8c02e81d7437
lyra-school <s00233718@atu.ie>
2024-11-14 19:23:28 +0000

    Add AWS SDK

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,6 +16,7 @@ dependencies {
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation(platform("software.amazon.awssdk:bom:2.29.12"))
 }
 
 tasks.test {
